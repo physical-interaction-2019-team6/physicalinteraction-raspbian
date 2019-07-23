1f8d9cb0f05cc79e9f1089419d3f9b5d691227da
ken-kentan <ken.kentan.k2@gmail.com>
2019-07-23 21:47:11 +0900

    led blink

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
 def main():
-    print("Hello")
+    print("LED blink")
+
+    while True:  # Run forever
+        GPIO.output(8, GPIO.HIGH)  # Turn on
+        print("HIGH")
+        sleep(1)  # Sleep for 1 second
+        GPIO.output(8, GPIO.LOW)  # Turn off
+        print("LOW")
+        sleep(1)  # Sleep for 1 second
 
 
 if __name__ == "__main__":
