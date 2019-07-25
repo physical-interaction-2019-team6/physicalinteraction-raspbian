18220ae1dab93e317c2ee31ee41a35fd64b019f6
mittsolo8 <mitu.kz.cog@gmail.com>
2019-07-25 17:35:18 +0900

    temporary

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-def main():
-    print("Hello")
-    print("Hello")
+from time import sleep
 
+import RPi.GPIO as GPIO
+
+
+def main():
+    motorA = 7  # Pin number,motorA
+    motorB = 8  # Pin number,motorB
+    emo_my = 0  # My emotion,not happy=0,happy=1
+    emo_ano = 0  # Another people emotion,not happy=0,happy=1
+    print("Motor")
+    GPIO.setwarnings(False)  # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+    GPIO.setup(motorA, GPIO.OUT, initial=GPIO.LOW)  # Set pin 7 to be an output pin and set initial value to low (off)
+    GPIO.setup(motorB, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low
+    while True:
+        if emo_my == emo_ano and 1 == emo_my:
+            GPIO.output(motorA, GPIO.HIGH)  # Turn on
+            print("MotorA HIGH")
+            sleep(1)  # Sleep for 1 second
+            GPIO.output(motorA, GPIO.LOW)  # Turn off
+            print("MotorA LOW")
+            sleep(0.5)  # Sleep for 0.5 second
+            GPIO.output(motorB, GPIO.HIGH)  # Turn on
+            print("MotorB HIGH")
+            sleep(1)  # Sleep for 1 second
+            GPIO.output(motorB, GPIO.LOW)  # Turn off
+            print("MotorB LOW")
+            sleep(1)  # Sleep for 1 second
 
 
 if __name__ == "__main__":
