db9d07fc100829e88e3b13926d06ce5dbb44109c
ken-kentan <ken.kentan.k2@gmail.com>
2019-07-26 09:59:11 +0900

    add Vibrator class

---
+++ vibrator.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
+class Vibrator:
+    def __init__(self):
+        self.pinMotorA = 7
+        self.pinMotorB = 8
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pinMotorA, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.pinMotorB, GPIO.OUT, initial=GPIO.LOW)
+
+    def vibrate(self):
+        print("vibrate")
+        GPIO.output(self.pinMotorA, GPIO.HIGH)
+        sleep(0.5)
+        GPIO.output(self.pinMotorA, GPIO.LOW)
+        GPIO.output(self.pinMotorB, GPIO.HIGH)
+        sleep(0.5)
+        GPIO.output(self.pinMotorB, GPIO.LOW)
