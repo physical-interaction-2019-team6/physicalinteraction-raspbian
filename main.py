7546e79e812fb20ab33fc4d6f2e4be9d40d874d7
ken-kentan <ken.kentan.k2@gmail.com>
2019-07-27 17:54:03 +0900

    migrate

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from time import sleep
+import time
+
+from communication import Communication
+from emotion_prediction import EmotionPrediction
+from recorder import Recorder
+from vibrator import Vibrator
 
 
 def main():
+    recorder = Recorder()
+    vibrator = Vibrator()
+
+    prediction = EmotionPrediction()
+    prediction.load_model()
+
     com = Communication()
-    while True:
-        com.send("Hello")
-        print(com.receive())
-        sleep(1)
+
+    recorder.start()
+    try:
+        time.sleep(1)
+        buf, rate = recorder.get_as_numpy_array()
+
+        is_happy = prediction.predict(buf, rate)
+        com.send(is_happy)
+
+        is_target_happy = com.receive()
+
+        print("is_happy: " + is_happy + "is_target_happy: " + is_target_happy)
+
+        if is_happy and is_target_happy:
+            vibrator.vibrate()
+    except KeyboardInterrupt:
+        pass
+
+    com.close()
+    recorder.stop()
 
 
 if __name__ == "__main__":
